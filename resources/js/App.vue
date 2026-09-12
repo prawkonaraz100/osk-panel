230fb09bfba611a265f7a1a5e46c4d28ef81bb81
prawkonaraz100 <prawkonaraz100@gmail.com>
2026-09-12 04:01:43 +0200

    CORE-V1-INTERNAL-EXAMS-001: promote internal exam management UI gate

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
 import CalendarWorkspace from './modules/CalendarTraining/CalendarWorkspace.vue'
+import InternalExamWorkspace from './modules/InternalExams/InternalExamWorkspace.vue'
+import LearningAccessWorkspace from './modules/LearningAccess/LearningAccessWorkspace.vue'
 import ResourceWorkspace from './modules/ResourcesCore/ResourceWorkspace.vue'
 import StudentCourseWorkspace from './modules/StudentsCourses/StudentCourseWorkspace.vue'
-import LearningAccessWorkspace from './modules/LearningAccess/LearningAccessWorkspace.vue'
 
 const isCalendarRoute = window.location.pathname === '/kalendarz'
 const isStudentCourseRoute = window.location.pathname === '/kursanci'
   || window.location.pathname.startsWith('/kursanci/')
 const isLearningAccessRoute = window.location.pathname === '/licencje/panel'
+const isInternalExamRoute = window.location.pathname === '/egzamin-wewnetrzny/panel'
 </script>
 
 <template>
   <CalendarWorkspace v-if="isCalendarRoute" />
+  <InternalExamWorkspace v-else-if="isInternalExamRoute" />
   <LearningAccessWorkspace v-else-if="isLearningAccessRoute" />
   <StudentCourseWorkspace v-else-if="isStudentCourseRoute" />
   <ResourceWorkspace v-else />
