1b4795dbe1d8c9a3ee5bb251cfd0686a0547b8d4
prawkonaraz100 <prawkonaraz100@gmail.com>
2026-09-13 21:33:12 +0200

    CORE-V1-STAGE4: recover commerce lineage prerequisite before write-fence

--- tests/Unit/MigrationPlanContractTest.php
+++ tests/Unit/MigrationPlanContractTest.php
@@ -18,7 +18,7 @@ class MigrationPlanContractTest extends TestCase
         $this->assertSame(157, $plan->implementedNodeCount());
         $this->assertSame(157, $plan->implementedStepCount());
         $this->assertSame('ad5f2aa2e14ef248b95dd3dda0d1cbcb2e69d441', $plan->summary()['authority_blob']);
-        $this->assertSame('d2da7eb3ccb082ca6b0106fbe4b1e543815c9236cc1a8bd62cf470c93459b660', $plan->executionIdentity());
+        $this->assertSame('7cdea7410b15a1e1ef884e50d4e2a9ac519eb568569d140f82b19cc5deaa142e', $plan->executionIdentity());
         $this->assertSame([
             'MIG-EXT-BTREE-GIST',
             'MIG-TBL-ORGANIZATIONS',
