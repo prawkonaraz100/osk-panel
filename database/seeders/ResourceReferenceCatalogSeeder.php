a5a376c3cf967ae2ec72d9856e94b04cb9164be5
prawkonaraz100 <prawkonaraz100@gmail.com>
2026-09-13 08:31:11 +0200

    FORMAL-DOC-008: isolate formal template seed from foundation catalog

--- database/seeders/ResourceReferenceCatalogSeeder.php
+++ database/seeders/ResourceReferenceCatalogSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Modules\FormalDocuments\FormalTrainingDocumentRenderer;
 use App\Modules\InternalExams\InternalExamAnswerSheetRenderer;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -151,24 +150,6 @@ final class ResourceReferenceCatalogSeeder extends Seeder
                 'created_at' => now(),
             ]);
 
-            foreach ([
-                ['0199f88d-8d00-7000-8000-000000000101', 'training_record_card'],
-                ['0199f88d-8d00-7000-8000-000000000102', 'theory_delivery_journal'],
-            ] as [$templateId, $documentType]) {
-                DB::table('formal_training_document_templates')->insertOrIgnore([
-                    'id' => $templateId,
-                    'document_type' => $documentType,
-                    'template_version' => 'v1',
-                    'renderer_version' => FormalTrainingDocumentRenderer::RENDERER_VERSION,
-                    'template_content_hash' => hash(
-                        'sha256',
-                        'prawkonaraz|formal_training_document|'.$documentType.'|v1|'.FormalTrainingDocumentRenderer::RENDERER_VERSION,
-                    ),
-                    'effective_from' => '2026-01-01 00:00:00+00',
-                    'effective_to' => null,
-                    'created_at' => now(),
-                ]);
-            }
 
             foreach (self::AUDIT_ACTIONS as $action) {
                 DB::table('audit_action_policy_revisions')->updateOrInsert(
