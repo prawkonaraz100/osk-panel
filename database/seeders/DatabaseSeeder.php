fb04c164254d06550ea1337e462b1d2e70971cc2
prawkonaraz100 <prawkonaraz100@gmail.com>
2026-09-13 08:31:26 +0200

    FORMAL-DOC-008: seed formal templates in production bootstrap

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             FoundationReferenceCatalogSeeder::class,
             ResourceReferenceCatalogSeeder::class,
+            FormalTrainingDocumentTemplateSeeder::class,
         ]);
     }
 }
