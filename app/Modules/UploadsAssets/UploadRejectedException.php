0c5f18b902c3c81241fc85446bd0f1a88d28dafa
prawkonaraz100 <prawkonaraz100@gmail.com>
2026-09-13 10:23:30 +0200

    FORMAL-DOC-010: carry committed upload rejection status

--- app/Modules/UploadsAssets/UploadRejectedException.php
+++ app/Modules/UploadsAssets/UploadRejectedException.php
@@ -9,6 +9,8 @@ final class UploadRejectedException extends RuntimeException
     public function __construct(
         string $message,
         public readonly string $assetId,
+        public readonly string $machineCode = 'VALIDATION_FAILED',
+        public readonly int $httpStatus = 422,
     ) {
         parent::__construct($message);
     }
