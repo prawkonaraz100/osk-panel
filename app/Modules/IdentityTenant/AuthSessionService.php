f08396fed68515df034a26951d52e3763a519812
prawkonaraz100 <prawkonaraz100@gmail.com>
2026-09-14 07:45:54 +0200

    CORE-V1-AUTH-SESSION-LIFECYCLE-001: fix static analysis import

--- app/Modules/IdentityTenant/AuthSessionService.php
+++ app/Modules/IdentityTenant/AuthSessionService.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Str;
+use LogicException;
 
 final class AuthSessionService
 {
