7beb93c9bf153f5ee4bac9d3e23999d18b449a66
prawkonaraz100 <prawkonaraz100@gmail.com>
2026-09-15 20:33:49 +0200

    REGISTRATION-UI-001: route registration workspace

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -3,6 +3,7 @@ import CalendarWorkspace from './modules/CalendarTraining/CalendarWorkspace.vue'
 import DashboardWorkspace from './modules/CommerceDashboard/DashboardWorkspace.vue'
 import PurchaseHistoryWorkspace from './modules/CommerceDashboard/PurchaseHistoryWorkspace.vue'
 import AuthWorkspace from './modules/IdentityTenant/AuthWorkspace.vue'
+import RegistrationWorkspace from './modules/IdentityTenant/RegistrationWorkspace.vue'
 import InternalExamWorkspace from './modules/InternalExams/InternalExamWorkspace.vue'
 import LearningAccessWorkspace from './modules/LearningAccess/LearningAccessWorkspace.vue'
 import SettingsWorkspace from './modules/OrganizationSettings/SettingsWorkspace.vue'
@@ -10,6 +11,7 @@ import ResourceWorkspace from './modules/ResourcesCore/ResourceWorkspace.vue'
 import StudentCourseWorkspace from './modules/StudentsCourses/StudentCourseWorkspace.vue'
 
 const isAuthRoute = ['/login', '/forgot-password', '/reset-password'].includes(window.location.pathname)
+const isRegistrationRoute = window.location.pathname === '/register'
 const isDashboardRoute = window.location.pathname === '/'
 const isPurchaseHistoryRoute = window.location.pathname === '/historia-zakupow'
 const isCalendarRoute = window.location.pathname === '/kalendarz'
@@ -21,7 +23,8 @@ const isSettingsRoute = window.location.pathname === '/ustawienia'
 </script>
 
 <template>
-  <AuthWorkspace v-if="isAuthRoute" />
+  <RegistrationWorkspace v-if="isRegistrationRoute" />
+  <AuthWorkspace v-else-if="isAuthRoute" />
   <DashboardWorkspace v-else-if="isDashboardRoute" />
   <PurchaseHistoryWorkspace v-else-if="isPurchaseHistoryRoute" />
   <CalendarWorkspace v-else-if="isCalendarRoute" />
